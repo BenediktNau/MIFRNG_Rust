7d47d3bea21f46a96ca034cb7ce8655dbecdef59
BenediktNau <benedikt@nausseite.de>
2024-12-04 16:52:48 +0000

    Created basics for LFSR/ Made it shift and create new states

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,48 @@
 use std::io;
 
-fn lfsr_step(input_nb: String, ){
-    let mut wert:u32 = 000000000000;
 
-    //get binary of input string
-    match input_nb.parse::<u32>() {
-        Ok(num) => {
-            wert = num;
-            let binary = format!("{:024b}", num);
-            println!("The 24-bit binary representation is: {}", binary);
-        }
-        Err(_) => {
-            println!("Invalid input. Please enter a valid positive decimal number.");
+
+
+fn lfsr(mut state: u32, polynomial: u32, num_bits: usize) -> (u32, u32) {
+    let mut output_bit = 0;
+    
+    
+    output_bit = (state & 1) as u32;
+
+    
+    let mut feedback = 0;
+    for i in 0..num_bits {
+        if (polynomial >> i) & 1 == 1 {
+            feedback ^= (state >> i) & 1;
         }
     }
 
-    let lsbs = wert & 1;
-
-    print!("lsb:", lsbs);
-
-    wert >>= 1;
+    print!("XOR-Bit: {}, ", feedback);
+    // shift everythin to right and add cor_bit at the begining
+    state = (state >> 1) | (feedback << (num_bits - 1));
     
-    println!("Wert: {}", format!("{:024b}", wert));
+    
+    (state, output_bit)
 }
 
-
-
-
 fn main() {
-    let mut input = String::new();
-
-    println!("Put in ur Startwert!");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let trimmed_input: String = input.trim().repeat(1);
-
-    lfsr_step(trimmed_input);
-
-
     
 
 
-    //Left shifCuts last 32 bits
-    //wert = wert << 1;
-    //let wert_cut = wert & 0xFFFFFF;
-    
+    let polynomial: u32 = 0b010110110110010101001001; 
 
+    let mut state: u32 = 0b100001111100001001100100; 
+
+    let num_bits = 24;
+
+
+    for i in 0..11 {
+        let (new_state, output_bit) = lfsr(state, polynomial, num_bits);
+        
+        let decimal_value = new_state;
+        
+
+        println!("State {:<2}, {:<24}, {:<9}", i , format!("{:024b}", new_state), decimal_value, );
+        state = new_state;
+    }
 }
